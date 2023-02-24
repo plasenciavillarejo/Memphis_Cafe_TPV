f90f0f4bcff5d7ccdaeb517f2ec7057a07e63b5b
joseplasenciavillarejo <plasenciavillarejo@gmail.com>
2023-02-24 19:47:20 +0100

    Se empieza a construir el diseño de la vista principal

--- src/main/java/com/memphis/cafe/tpv/controllers/MemphisController.java
+++ src/main/java/com/memphis/cafe/tpv/controllers/MemphisController.java
@@ -1,10 +1,18 @@
 package com.memphis.cafe.tpv.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.memphis.cafe.tpv.entity.LogosIniciales;
+import com.memphis.cafe.tpv.service.IBebidasService;
 
 @Controller
 @SessionAttributes("memphisCafe")
@@ -12,8 +20,21 @@ public class MemphisController {
 
 	private Logger logAplicacion = LoggerFactory.getLogger(this.getClass());
 
+	@Autowired
+	private IBebidasService bebidasService;
+	
 	@GetMapping({ "/inicio", "/" })
-	public String inicio() {
+	public String inicio(Model model) {
+		
+		List<LogosIniciales> localizarNombre = new ArrayList<>();
+		localizarNombre = bebidasService.buscarLogosPrincipales();
+		
+		for(LogosIniciales logo : localizarNombre) {
+			if(!logo.getNombre().isEmpty()) {
+				model.addAttribute("logosPrincipales", logo.getNombre());
+			}
+		}
+		
 		logAplicacion.info("Mostrando la carta para el Cafe Bar - Memphis");
 		return "inicio";
 	}
